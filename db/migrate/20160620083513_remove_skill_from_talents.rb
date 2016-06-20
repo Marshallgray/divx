226d8924a6fc778535aa705cb06a881115883362
Heath Garvey <heathgarvey@gmail.com>
2016-06-20 19:12:16 +1000

    Cured all tables

--- db/migrate/20160620083513_remove_skill_from_talents.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSkillFromTalents < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :talents, :skill_type, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620084519) do
+ActiveRecord::Schema.define(version: 20160617063452) do
 
   create_table "business_locations", force: :cascade do |t|
     t.string   "title"
@@ -93,11 +93,6 @@ ActiveRecord::Schema.define(version: 20160620084519) do
     t.datetime "updated_at"
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
-  end
-
-  create_table "skill_types", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "talents", force: :cascade do |t|
